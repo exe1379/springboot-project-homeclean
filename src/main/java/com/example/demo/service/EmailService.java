369040beb43b0828c7ffa5aad5ccbfa5cfec07bf
Zenboo 13 <Zenboo 13@DESKTOP-N0S4I95>
2025-06-04 16:56:14 +0800

    mail

--- src/main/java/com/example/demo/service/EmailService.java
+++ src/main/java/com/example/demo/service/EmailService.java
@@ -1,44 +1,30 @@
 package com.example.demo.service;
 
-import jakarta.mail.*;
-import jakarta.mail.internet.*;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.*;
 import org.springframework.stereotype.Service;
-
-import java.util.Properties;
 
 @Service
 public class EmailService {
 
-    // Google應用程式密碼
-    private final String googleAppPassword = "kqnz ctfa fmps xyas";
-
-    // 寄件者的 Gmail 帳號
-    private final String from = "exe1379@gmail.com";
+    @Autowired
+    private JavaMailSender mailSender;
 
     public void sendEmail(String to, String confirmUrl) {
-        String host = "smtp.gmail.com";
-
-        Properties properties = new Properties();
-        properties.put("mail.smtp.auth", "true");
-        properties.put("mail.smtp.starttls.enable", "true");
-        properties.put("mail.smtp.host", host);
-        properties.put("mail.smtp.port", "587");
-
-        Session session = Session.getInstance(properties, new Authenticator() {
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(from, googleAppPassword);
-            }
-        });
-
         try {
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(from));
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
-            message.setSubject("會員註冊確認信");
-            message.setText("請點選以下連結進行帳號驗證：\n" + confirmUrl);
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, false, "UTF-8");
 
-            Transport.send(message);
+            helper.setTo(to);
+            helper.setSubject("會員註冊確認信");
+            helper.setText("請點選以下連結進行帳號驗證：\n" + confirmUrl, false);
+            helper.setFrom("exe1379@gmail.com");
+
+            mailSender.send(message);
             System.out.println("信件已寄出：" + to);
+
         } catch (MessagingException e) {
             System.out.println("寄信失敗：" + e.getMessage());
         }
